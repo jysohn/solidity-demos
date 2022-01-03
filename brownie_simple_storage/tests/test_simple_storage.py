555f24687c8d1d009680091635734eb2d9444704
Jae Sohn <jaeyoung.sohn@gmail.com>
2022-01-03 17:49:01 +0900

    adding tests

--- brownie_simple_storage/tests/test_simple_storage.py
+++ brownie_simple_storage/tests/test_simple_storage.py
@@ -2,6 +2,18 @@ from brownie import SimpleStorage, accounts
 
 def test_deploy():
     # Arrange
+    account = accounts[0]
     # Act
+    simple_storage = SimpleStorage.deploy({"from": account})
+    starting_value = simple_storage.retrieve()
+    expected = 0
     # Assert
-    pass
+    assert starting_value == expected
+
+def test_store():
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    simple_storage.store(5, {"from": account})
+    new_value = simple_storage.retrieve()
+    expected = 5
+    assert new_value == expected
